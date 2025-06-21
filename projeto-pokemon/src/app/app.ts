fbbafcf4079fc75604c5f1238b182e0a12ea2372
nikolau96 <nickolascosta35@gmail.com>
2025-06-21 11:33:00 -0300

    Comunicação com a API

--- projeto-pokemon/src/app/app.ts
+++ projeto-pokemon/src/app/app.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PokeData } from './poke-data';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
+  providers: [PokeData]
 })
-export class App {
+export class App implements OnInit {
   protected title = 'projeto-pokemon';
+  poke: any;
+  url = 'https://pokeapi.co/api/v2/pokemon';
+  constructor (private service: PokeData){}
+  getPoke():void{
+    this.service.getData(this.url).subscribe(data=>this.poke.data);
+  }
+  ngOnInit(){
+    this.getPoke();
+  }
 }
